bed113e2e564497fc24b3a7953055f0cd55d8643
Bela Varga <kitudja@gmail.com>
2026-02-05 20:36:39 +0100

    delete more unneeded files

--- reference/AudioLibSearch/AudioLibSearch.types.ts
+++
@@ -1,9 +0,0 @@
-export type AudioLibSearchElement = {
-  id: string;
-  youtubeLink: string;
-  spotifyLink: string;
-  timestamp: number;
-  title: string;
-  description: string;
-  tags: string[];
-};

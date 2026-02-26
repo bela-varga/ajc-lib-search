8428d87633616c5ce7ee1b3b4b3a902a585dd427
Bela Varga <kitudja@gmail.com>
2026-02-26 02:42:18 +0100

    fix react router config for github pages

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,9 +1,8 @@
 import type { Config } from '@react-router/dev/config';
 
 export default {
-  // Config options...
-  // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: true,
+  ssr: false,
+  basename: '/ajc-lib-search/',
   async prerender() {
     return ['/', '/about', '/contact', '/terms', '/privacy'];
   },
